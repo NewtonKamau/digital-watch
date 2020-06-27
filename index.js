00779b93d436cebf64d37804505f59f0df9ccf7b
Newton <kamaunewton78@gmail.com>
2020-06-27 11:16:18 +0300

    set our time using setInterval()

---
+++ index.js
@@ -0,0 +1,17 @@
+setInterval(() => {
+    let time = document.querySelector('p');
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+
+    if (h < 10)
+        h = "0" + h;
+    if (m < 10)
+        m = "0" + m;
+    if (s < 0)
+        s = "0" + s;
+    if (h => 12)
+        time.innerText = ` ${h} : ${m} : ${s} PM` ;
+    if (h < 12)
+        time.innerText = `${h} : ${m} : ${s} AM` ;
+},1000);
